3069113b7cec0e85ffe8583a186369d263b4503d
allan-almeida1 <allan.almeida@ufba.br>
2024-08-16 08:39:01 -0300

    add missing docstrings

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@ from app.views import main, ketcher, api
 
 def create_app():
     """Create the Flask app."""
+
     app = Flask(__name__)
-    app.config.from_object("config.Config")
     app.register_blueprint(main)
     app.register_blueprint(ketcher, url_prefix='/ketcher')
     app.register_blueprint(api, url_prefix='/api/v1')
 
+    # Error handling
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html'), 404
