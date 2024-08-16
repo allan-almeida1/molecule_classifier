04808cac827317c0df758caabd7c630086420ed9
allan-almeida1 <allan.almeida@ufba.br>
2024-08-16 00:40:07 -0300

    predictor setup finished

--- app/views.py
+++ app/views.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, send_from_directory, render_template, jsonify
+from flask import Blueprint, send_from_directory, render_template
+from flask import jsonify, request
+import numpy as np
+from predictor import Classifier
+from lib.descriptor_gen import DescriptorGen
+from rdkit import RDLogger
+RDLogger.DisableLog('rdApp.*')
 
 main = Blueprint('main', __name__)
 
@@ -31,9 +37,35 @@ api = Blueprint('api', __name__)
 
 @api.route('/molecule-classifier/predict', methods=['POST'])
 def predict():
+    # Get the input data
+    input_data = request.json
+    smiles = input_data['smiles']
+
+    if smiles == '':
+        response = {
+            'success': False,
+        }
+        return jsonify(response)
+
+    # Generate the molecular descriptors
+    desc_gen = DescriptorGen()
+    desc = desc_gen.from_smiles(smiles)
+    desc = np.stack(desc).reshape(1, -1)
+
+    # Load the model
+    model = Classifier()
+
+    # Make prediction
+    prediction = model.predict(desc)
+
+    # Get the prediction class with the highest probability
+    prediction_class = int(np.argmax(prediction))
+
+    # Prepare the response
+    label_map = {0: 'Activator', 1: 'Inactive', 2: 'Inhibitor'}
     response = {
         'success': True,
-        'prediction_id': 1,
-        'prediction_class': 'Inactive'
+        'prediction_id': prediction_class,
+        'prediction_class': label_map[prediction_class]
     }
     return jsonify(response)
