7880ccf8a451096f5588fcbacef9a43cdb103865
allan-almeida1 <allan.almeida@ufba.br>
2024-08-15 22:08:17 -0300

    fetch numbers api asynchronously and update UI

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,4 +1,24 @@
+function showLoading() {
+  let helperText = document.getElementById("helperText");
+  let loader = document.getElementById("loaderDots");
+  let button = document.getElementById("send");
+  loader.style.display = "block";
+  helperText.textContent = "Processing";
+  button.disabled = true;
+}
+
+function showDone() {
+  let helperText = document.getElementById("helperText");
+  let loader = document.getElementById("loaderDots");
+  let button = document.getElementById("send");
+  loader.style.display = "none";
+  helperText.textContent = "Click the button to classify the molecule";
+  button.disabled = false;
+}
+
 async function sendMolecule() {
+  showLoading();
+
   let ketcherFrame = document.getElementById("ifKetcher");
   let ketcher = null;
 
@@ -9,7 +29,17 @@ async function sendMolecule() {
     ketcher = document.frames["ifKetcher"].window.ketcher;
   }
 
+  // Get the molecule in Smiles format
   const molecule = await ketcher.getSmiles();
 
-  console.log(molecule);
+  // Send to the backend
+  fetch("http://numbersapi.com/random").then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    response.text().then((data) => {
+      console.log(data);
+      showDone();
+    });
+  });
 }
