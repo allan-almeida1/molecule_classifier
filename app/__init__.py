ef873e9e76a72b36574d49c224831eb1878e1e78
allan-almeida1 <allan.almeida@ufba.br>
2024-08-15 21:02:22 -0300

    implement web home page with styles

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.views import main
+from app.views import main, ketcher
 
 
 def create_app():
@@ -7,5 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
     app.register_blueprint(main)
+    app.register_blueprint(ketcher, url_prefix='/ketcher')
 
     return app
--- app/views.py
+++ app/views.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, send_from_directory, render_template
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@main.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+ketcher = Blueprint('ketcher', __name__)
+
+
+@ketcher.route('/', methods=['GET'])
+def serve_ketcher():
+    return send_from_directory('static/ketcher', 'index.html')
+
+
+@ketcher.route('/static/<path:filename>', methods=['GET'])
+def serve_ketcher_static(filename):
+    return send_from_directory('static/ketcher/static', filename)
