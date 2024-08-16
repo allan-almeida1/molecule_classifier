ced938f9995894fb4eca9eea44a693b16fdf44db
allan-almeida1 <allan.almeida@ufba.br>
2024-08-16 08:52:33 -0300

    document JavaScript code

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,14 +1,28 @@
+/**
+ * Show a welcome dialog to the user on the home page
+ */
 function showDialog() {
   let dialog = document.getElementById("welcomeDialog");
   dialog.showModal();
 }
 
+/**
+ * Verify local storage for the `dontShowDialog` flag.
+ *
+ * If it's not found, show the dialog
+ */
 if (localStorage.getItem("dontShowDialog") !== "true") {
   setTimeout(() => {
     showDialog();
   }, 2000);
 }
 
+/**
+ * Close the welcome dialog.
+ *
+ * It the checkbox is checked, set an item in local storage to prevent
+ * the dialog to show up again
+ */
 function closeDialog() {
   const dontShowAgainCheckbox = document.getElementById("dontShow");
   if (dontShowAgainCheckbox.checked) {
@@ -18,6 +32,10 @@ function closeDialog() {
   dialog.close();
 }
 
+/**
+ * Change the visibility and content of UI elements to show the request
+ * is bein processed
+ */
 function showLoading() {
   let helperText = document.getElementById("helperText");
   let loader = document.getElementById("loaderDots");
@@ -29,6 +47,18 @@ function showLoading() {
   button.disabled = true;
 }
 
+/**
+ * @typedef {Object} PredictResponse
+ * @property {boolean} success Whether the prediction was successful.
+ * @property {number} prediction_id The ID of the predicted class.
+ * @property {string} prediction_class The name of the predicted class.
+ */
+
+/**
+ *
+ * @param {PredictResponse} data The server's JSON response
+ * `success`
+ */
 function showResult(data) {
   let helperText = document.getElementById("helperText");
   let loader = document.getElementById("loaderDots");
@@ -47,9 +77,15 @@ function showResult(data) {
     : "Unknown";
 }
 
+/**
+ * Get the molecule SMILES from Ketcher app and send it to the server.
+ *
+ * Once the response has been received, display it on the page
+ */
 async function sendMolecule() {
   showLoading();
 
+  // Get Ketcher object
   let ketcherFrame = document.getElementById("ifKetcher");
   let ketcher = null;
 
@@ -63,11 +99,16 @@ async function sendMolecule() {
   // Get the molecule in Smiles format
   const molecule = await ketcher.getSmiles();
 
+  // Prepare the data
   const data = {
     smiles: molecule,
   };
 
-  // Send to the backend
+  /**
+   * Send to the server
+   *
+   * @note `apiUrl` is dinamically obtained in base.html template
+   */
   fetch(apiUrl, {
     method: "POST",
     headers: {
